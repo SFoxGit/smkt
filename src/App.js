3ef293acc2fe8c3df38d8ed935b222d5ab24df8a
Shawn Fox <foxeasttrojan06@gmail.com>
2021-05-01 20:08:15 -0400

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
+import Menu from './components/Menu';
+import Events from './components/Events';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/menu">
+          <Menu />
+        </Route>
+        <Route exact path="/events">
+          <Events />
+        </Route>
+        <Route>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
