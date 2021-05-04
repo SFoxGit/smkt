eacba8931b6823a49e61d34d76bb5dfdcc9d5895
Shawn Fox <foxeasttrojan06@gmail.com>
2021-05-03 22:49:50 -0400

    homepage setup

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Col, Container, Image, Row } from "react-bootstrap";
 
 function Home() {
 
   return (
     <>
+      <Container>
+        <Row className="justify-content-center text-light">
+          <h2 style={{textAlign: "center"}}>South Main Kitchen & Tavern</h2>
+        </Row>
+        <Row>
+          <Col xs={12} md={6} className="mb-4">
+            <Image fluid src="./images/beeftips.jpg"/>
+          </Col>
+          <Col xs={12} md={6} className="mb-4">
+            <Image fluid src={"./images/flounder.jpg"}/>
+          </Col>
+          <Col xs={12} md={6} className="mb-4">
+            <Image fluid src={"./images/gyro.jpg"}/>
+          </Col>
+          <Col xs={12} md={6} className="mb-4">
+            <Image fluid src={"./images/burger.jpg"}/>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
