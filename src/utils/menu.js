a97c1589c9fb2f37413c5fd4e56f18734f73f174
Shawn Fox <foxeasttrojan06@gmail.com>
2021-05-04 00:09:53 -0400

    menu array

---
+++ src/utils/menu.js
@@ -0,0 +1,400 @@
+const menu = [
+  // Starters
+{
+name: "Homemade Pimento Cheese Dip",
+description: "Classic Pimento cheese recipe served with fried tortilla chips.",
+price: "$8.45"
+},
+{
+name: "Cheesy Spinach Artichoke Dip",
+description: "Served with fried tortilla chips.",
+price: "$8.45"
+},
+{
+name: "Fried Mozzarella Sticks",
+description: "Served with marinara.",
+price: "$7.45"
+},
+{
+name: "Fried Green Tomatoes",
+description: "Buttermilk soaked and fried topped with brown sugar bacon crumbles along with a side of Caj…",
+price: "$7.95"
+},
+{
+name: "South Main Shrimp",
+description: "Fried shrimp drizzled with a sweet Thai aioli topped with sesame seeds",
+price: "$8.45"
+},
+{
+name: "Main Street Cocktail",
+description: "Jumbo shrimp served with house-made cocktail sauce.",
+price: "$9.25"
+},
+{
+name: "Wings",
+price: "$7.95",
+},{
+name: "Potato Skins",
+description: "Thick cut potato halves cover with cheese and bacon",
+price: "$7.95"
+},
+// "Salads & Soups"
+{
+name: "Avocado Chicken Cobb Salad",
+description: "Iceberg and Romaine mix topped with diced tomato, bacon, eggs, avocado with Bleu chees…",
+price: "$11.95"
+},
+{
+name: "Roadhouse Chicken Salad",
+description: "Iceberg and Romaine mix topped with diced tomato, cucumbers, chopped egg,bacon, shred…",
+price: "$10.95"
+},
+{
+name: "Greek Salad",
+description: "Iceberg and Romaine mix topped with diced tomato, cucumbers, red onion, pepperoncini, K…",
+price: "$8.45"
+},
+{
+name: "Main Street Salad",
+description: "Iceberg and Romaine mix topped with steak tips, diced tomato, cucumbers, red onion, bleu …",
+price: "$11.95"
+},
+{
+name: "Chef Salad",
+description: "Iceberg and Romaine mix topped with diced tomato,cucumbers,egg, shredded cheese, ham…",
+price: "$9.95"
+},
+{
+name: "Soup & Salad",
+description: "16oz Bowl of Soup with a House or Caesar Salad. Please call 704-812-8973 to find out th…",
+price: "$7.95"
+},
+{
+name: "House Salad",
+description: "Iceburg and Romaine lettuce mix with cucumbers, diced tomatoes, red onion and shr…",
+price: "$3.95"
+},
+{
+name: "Entree Caesar Salad",
+price: "$7.95",
+
+},{
+name: "Soup",
+description: "Please call 704-812-8973 to find out the soup of the day.",
+price: "$2.95"
+},
+{
+name: "Burgers",
+description: "South Main Burger Lettuce, tomato, cheese and onion on a toasted bun. Served with one side.",
+price: "$9.95"
+},
+{
+name: "Carolina Burger",
+description: "Two 4oz patties with chili, slaw, mustard, cheese and onion. Served with one side.",
+price: "$9.45"
+},
+{
+name: "Pita Burger",
+description: "Two 4oz patties with lettuce, tomato, cheese and mayo. Served with one side.",
+price: "$9.95"
+},
+{
+name: "Mushroom Swiss Burger",
+description: "Mushrooms,grilled onions and Swiss cheese on a toasted bun. Served with one side.",
+price: "$10.95"
+},
+{
+name: "Mt. Holly Burger",
+description: "Pimento cheese, bacon, lettuce, tomato and onion on a toasted bun. Served with one side.",
+price: "$10.95"
+},
+// "Sandwiches"
+{
+name: "The Philly Cheesesteak",
+description: "Sliced Philly steak with grilled onions, peppers, mushrooms and mayo on a hoagie. Served wit…",
+price: "$10.95"
+},
+{
+name: "Buffalo Chicken Sandwich",
+description: "Fried chicken breast with lettuce and tomato served on a toasted bun. Served with one side.",
+price: "$9.95"
+},
+{
+name: "Gyro or Chicken Gyro",
+description: "Sliced gyro meat or chicken with diced tomato, onions and tzatziki sauce served on pita bread…",
+price: "$9.95"
+},
+{
+name: "Chicken Bacon Ranch Wrap",
+description: "Fried chicken, bacon, Mozzarella cheese and ranch dressing on a tortilla wrap. Served with o…",
+price: "$9.95"
+},
+{
+name: "Chicken Salad Pita",
+description: "Homemade chicken salad with lettuce and tomato on pita bread. Served with one side.",
+price: "$9.45"
+},
+{
+name: "Ribeye Steak Sandwich",
+description: "6 oz. Ribeye with lettuce and tomato on a toasted bun. Served with one side.",
+price: "$10.95"
+},
+{
+name: "Main Street Club",
+description: "Ham, turkey, bacon, American cheese, lettuce, tomato and mayo on white bread. Served with …",
+price: "$10.95"
+},
+// "Entrees"
+{
+name: "Gaston County Honey Chicken" ,
+description:  "Golden fried chicken breast drizzled with honey. Served with two sides.",
+price: "$11.45"
+},
+{
+name: "Dutchman’s Creek Chicken",
+description: "Grilled chicken breast with BBQ sauce, crumbled bacon and shredded cheese then finished in th…",
+price: "$11.95"
+},
+{
+name: "Marinated Chicken Breast Dinner",
+description: "Grilled chicken breast that has been house marinated served on a bed of rice. Served with …",
+price: "$10.95"
+},
+{
+name: "Chicken Tender Platter",
+description: "Chicken tenders hand breaded and fried golden brown. Served with two sides.",
+price: "$10.95"
+},
+{
+name: "Beef Tips or Chicken Tips",
+description: "House marinated and grilled with mushrooms, peppers and onions served over rice. Served w…",
+price: "$10.95"
+},
+{
+name: "Hamburger Steak",
+description: "10 oz. marinated ground beef topped with grilled onions and brown gravy. Served with tw…",
+price: "$11.95"
+},
+{
+name: "Pork Chops",
+description: "26 oz. Bone-in pork chops marinated and grilled. Served with two sides.",
+price: "$11.95"
+},
+{
+name: "Baby Beef Liver",
+description: "Grilled and served over a bed of rice with grilled onions and gravy. Served with two sides.",
+price: "$9.95"
+},
+{
+name: "House Smoked Ribs",
+description: "Slow cooked and finished in our signature barbeque sauce. Served with two sides.",
+price: "$16.95"
+},
+{
+name: "Pulled Pork Barbeque Plate",
+description: "Smoked in house and served with hushpuppies. Served with two sides.",
+price: "$10.95"
+},
+{
+name: "Calabash Chicken Basket",
+description: "Tender chunks of white meat chicken deep fried golden brown. Served with two sides.",
+price: "$10.95"
+},
+// "Seafood"
+{
+name: "Seafood Platter",
+description:  "Fried flounder, calabash shrimp, jumbo shrimp, scallops and deviled crab. Served with two sid…",
+price: "$17.95"
+},
+{
+name: "Fried Flounder",
+description: "Served with two sides and hush puppies.",
+price: "$11.45"
+},
+{
+name: "Calabash Shrimp",
+description: "Served with two sides and hush puppies.",
+price: "$11.95"
+},
+{
+name: "Salt & Peppered Catfish",
+description: "Served with two sides and hush puppies.",
+price: "$10.95"
+},
+{
+name: "Grilled Salmon",
+description: "Blackened or grilled over a bed of rice served with two sides.",
+price: "$13.45"
+},
+// "Italian"
+{
+name: "Spaghetti with Meat Sauce",
+description: "Traditional spaghetti with house made meat sauce sprinkled with Parmesan Cheese. Serve…",
+price: "$10.45"
+},
+{
+name: "Baked Lasagna",
+description: "House made lasagna covered with meat sauce and Mozzarella cheese. Served with salad and …",
+price: "$12.45"
+},
+{
+name: "Chicken Parmesan",
+description: "Fried white meat chicken covered in our house made meat sauce topped with Mozzarella. Ser…",
+price: "$12.45"
+},
+{
+name: "Chicken Pasta",
+description: "Tender chunks of chicken sauteed with mushrooms and broccoli, tossed in our house-…",
+price: "$13.45"
+},
+{
+name: "Seafood Pasta",
+description: "Shrimp, Scallops and Clams served over linguine in a marinara sauce",
+price: "$14.45"
+},
+// "Steaks"
+{
+name: "Marinated London Broil Strips",
+description: "Tender house marinated strips of London Broil served over rice along with your choice of two …",
+price: "$12.95"
+},
+{
+name: "16 oz Ribeye",
+description: "Served with two sides.",
+price: "$26.95"
+},
+{
+name: "12 oz. Ribeye",
+description: "Served with two sides.",
+price: "$21.95"
+},
+{
+name: "Sides",
+description: "Crispy Fries",
+price: "$2.45"
+},
+{
+name: "Rice & Gravy",
+price: "$2.45",
+
+},{
+name: "Steamed Broccoli",
+price: "$2.45",
+
+},{
+name: "Fried Squash",
+price: "$2.45",
+
+},{
+name: "Fried Okra",
+price: "$2.45",
+
+},{
+name: "Green Beans",
+price: "$2.45",
+
+},{
+name: "Pasta Salad",
+price: "$2.45",
+
+},{
+name: "Peaches & Cottage Cheese",
+price: "$2.45",
+
+},{
+name: "Apple Sauce",
+price: "$2.45",
+
+},{
+name: "Coleslaw",
+price: "$2.45",
+
+},{
+name: "Macaroni Salad",
+price: "$2.25",
+
+},{
+name: "Pickled Beets",
+price: "$2.45",
+
+},{
+name: "Cucumber Salad",
+price: "$2.45",
+
+},{
+name: "Onion Tanglers",
+price: "$2.45",
+
+},{
+name: "Potato Salad",
+price: "$2.45",
+
+},{
+name: "Collard Greens",
+price: "$2.45",
+
+},{
+name: "Onion Rings",
+price: "$2.45",
+
+},{
+name: "Mashed Potatoes & Gravy",
+price: "$2.45",
+
+},{
+name: "Baked Potato",
+price: "$2.45",
+
+},{
+name: "Beverages",
+description: "Soda",
+price: "$2.45"
+},
+{
+name: "Lemonade",
+price: "$2.45",
+
+},{
+name: "Iced Tea",
+price: "$2.45",
+
+},
+// "Kids Menu"
+{
+name: "Kid Cheeseburger",
+description: "4oz patty with American cheese on a bun.",
+price: "$4.50"
+},
+{
+name: "Kid Tenders",
+description: "2 hand-breaded chicken tenders fried golden brown. Also available grilled upon request.",
+price: "$4.50"
+},
+{
+name: "Kid Corn Dog",
+price: "$4.50",
+
+},{
+name: "Kid Grilled Cheese",
+price: "$4.50",
+
+},{
+name: "Kid Spaghetti",
+description: "Spaghetti noodles topped with our house-made meat sauce.",
+price: "$4.50"
+},
+{
+name: "Kid Alfredo",
+description: "Fettuccine tossed in our house-made Alfredo.",
+price: "$4.50"
+},
+{
+name: "Kid Flounder",
+description: "Fried filet of flounder served with one side.",
+price: "$5.50"
+},
+{
+name: "Kid Calabash Shrimp",
+price: "$5.50",
+},
+]
