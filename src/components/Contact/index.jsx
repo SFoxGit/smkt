775c39e90535e0f22c8fc008b80ab25312bbea4e
Shawn Fox <foxeasttrojan06@gmail.com>
2021-05-26 23:29:14 -0400

    modal

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import Container from 'react-bootstrap/Container'
-import { Row, Col, Button } from "react-bootstrap"
+import { Row, Col } from "react-bootstrap"
 import "./style.css"
 
 export default function Contact() {
@@ -15,7 +15,7 @@ export default function Contact() {
           <Row><a href="tel:7048128973">(704) 812-8973</a></Row>
           <Row></Row>
         </Col>
-        <Col xs={12} lg={6} style={{ height: "400px" }} >
+        {/* <Col xs={12} lg={6} style={{ height: "400px" }} >
           <Row className="border-bottom d-flex justify-content-center"><h2>Order</h2></Row>
           <Row>
             <Col xs={12} className="text-center mt-4">Pick-Up</Col>
@@ -25,7 +25,7 @@ export default function Contact() {
             <Col xs={6} className="d-flex justify-content-center"><Button className="orderBtn" href="https://www.doordash.com/store/south-main-kitchen-and-tavern-mount-holly-630460" target="_blank">Doordash</Button></Col>
             <Col xs={6} className="d-flex justify-content-center"><Button className="orderBtn" href="https://www.grubhub.com/restaurant/south-main-kitchen-and-tavern-128-s-main-st-mt-holly/1517396" target="_blank">GrubHub</Button></Col>
           </Row>
-        </Col>
+        </Col> */}
         <Col xs={12} lg={6} style={{ height: "400px" }}>
           <MapContainer style={{ height: "400px", width: "100%" }} center={[35.29629, -81.01604]} zoom={16} scrollWheelZoom={true}>
             <TileLayer
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { Navbar, Nav } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Navbar, Nav, Modal, Button, Row, Col } from 'react-bootstrap'
 import './style.css'
 
 function Header() {
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <Navbar collapseOnSelect expand="lg" variant="dark" sticky="top">
@@ -22,9 +25,34 @@ function Header() {
           <Nav.Link className="link col-xs-12 col-md-3" href="/menu">Menu</Nav.Link>
           <Nav.Link className="link col-xs-12 col-md-3" href="/events">Events</Nav.Link>
           <Nav.Link className="link col-xs-12 col-md-3" href="/contact">Contact Us</Nav.Link>
-          <Nav.Link className="link col-xs-12 col-md-3" href="/order">Order</Nav.Link>
+          <div className="link col-xs-12 col-md-3" onClick={handleShow}>Order</div>
         </Nav>
       </Navbar.Collapse>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+            <Row><h2>Order</h2></Row>
+        </Modal.Header>
+        <Modal.Body>
+          <Col xs={12} lg={6} style={{ height: "400px" }}>
+            <Row>
+              <Col xs={12} className="text-center mt-4">Pick-Up</Col>
+              <Col xs={6} className="d-flex justify-content-center"><Button className="orderBtn" href="tel:7048128973">Call</Button></Col>
+              <Col xs={6} className="d-flex justify-content-center"><Button className="orderBtn" href="https://direct.chownow.com/order/15163/locations" target="_blank">Online</Button></Col>
+              <Col xs={12} className="text-center mt-4">Delivery</Col>
+              <Col xs={6} className="d-flex justify-content-center"><Button className="orderBtn" href="https://www.doordash.com/store/south-main-kitchen-and-tavern-mount-holly-630460" target="_blank">Doordash</Button></Col>
+              <Col xs={6} className="d-flex justify-content-center"><Button className="orderBtn" href="https://www.grubhub.com/restaurant/south-main-kitchen-and-tavern-128-s-main-st-mt-holly/1517396" target="_blank">GrubHub</Button></Col>
+            </Row>
+          </Col>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Navbar>
   )
 }
