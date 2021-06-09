acea792a0361359475da990104b613568f71b077
Shawn Fox <foxeasttrojan06@gmail.com>
2021-06-08 23:22:32 -0400

    clean up

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -15,17 +15,6 @@ export default function Contact() {
           <Row><a href="tel:7048128973">(704) 812-8973</a></Row>
           <Row></Row>
         </Col>
-        {/* <Col xs={12} lg={6} style={{ height: "400px" }} >
-          <Row className="border-bottom d-flex justify-content-center"><h2>Order</h2></Row>
-          <Row>
-            <Col xs={12} className="text-center mt-4">Pick-Up</Col>
-            <Col xs={6} className="d-flex justify-content-center"><Button className="orderBtn" href="tel:7048128973">Call</Button></Col>
-            <Col xs={6} className="d-flex justify-content-center"><Button className="orderBtn" href="https://direct.chownow.com/order/15163/locations" target="_blank">Online</Button></Col>
-            <Col xs={12} className="text-center mt-4">Delivery</Col>
-            <Col xs={6} className="d-flex justify-content-center"><Button className="orderBtn" href="https://www.doordash.com/store/south-main-kitchen-and-tavern-mount-holly-630460" target="_blank">Doordash</Button></Col>
-            <Col xs={6} className="d-flex justify-content-center"><Button className="orderBtn" href="https://www.grubhub.com/restaurant/south-main-kitchen-and-tavern-128-s-main-st-mt-holly/1517396" target="_blank">GrubHub</Button></Col>
-          </Row>
-        </Col> */}
         <Col xs={12} lg={6} style={{ height: "400px" }}>
           <MapContainer style={{ height: "400px", width: "100%" }} center={[35.29629, -81.01604]} zoom={16} scrollWheelZoom={true}>
             <TileLayer
