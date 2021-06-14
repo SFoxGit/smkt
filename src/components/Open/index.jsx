8d5b875a66b2407958f9013353ba83aab4a99eba
Shawn Fox <foxeasttrojan06@gmail.com>
2021-06-13 20:38:34 -0400

    hours fix

--- src/components/Open/index.jsx
+++ src/components/Open/index.jsx
@@ -3,10 +3,12 @@ import {Container, Row, Col} from "react-bootstrap"
 
 export default function Open() {
   const d = new Date()
-  if (d === 0) {const hours = "11:00 am - 4:00 pm"}
-  if (d === 1) {const hours = "Closed on Mondays"}
-  if (d < 1 && d > 5) {const hours = "11:00 am - 9:00 pm"}
-  if (d > 4) {const hours = "11:00 am - 10:00 pm"}
+  const hours = () => {
+  if (d === 0) {return "11:00 am - 4:00 pm"}
+  if (d === 1) {return "Closed on Mondays"}
+  if (d < 1 && d > 5) {return "11:00 am - 9:00 pm"}
+  if (d > 4) {return "11:00 am - 10:00 pm"}
+  }
   return (
     <Container>
       <Row>
