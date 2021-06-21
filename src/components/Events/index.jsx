938abc7ce0ef42dc75ea0c664e36fc7a451bd21d
Shawn Fox <foxeasttrojan06@gmail.com>
2021-06-21 15:25:45 -0400

    event row col fix

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -5,23 +5,23 @@ function Events() {
   return (
     <Container>
       <Row style={{ padding: "10px" }}>
-          <Row className="border-bottom mb-4"><h2>Summer Concert Series</h2></Row>
-        <Col xs={12} lg={5} style={{  padding: "10px" }}>
+        <Col xs={12}><Row className="border-bottom mb-4"><h2>Summer Concert Series</h2></Row></Col>
+        <Col xs={12} lg={5} style={{ padding: "10px" }}>
           <Row>Our patio is the perfect place to enjoy the Mount Holly Summer Concert Series!</Row>
         </Col>
         <Col xs={0} lg={2}></Col>
         <Col xs={12} lg={5} style={{ height: "400px", padding: "10px" }} className="mb-3">
-          <Image style={{ height: "400px" , objectFit: "cover" }} src="../images/patio.jpg" roundedCircle fluid />
+          <Image style={{ height: "400px", objectFit: "cover" }} src="../images/patio.jpg" roundedCircle fluid />
         </Col>
       </Row>
       <Row style={{ padding: "10px" }}>
-          <Row className="border-bottom mb-4"><h2>Reserve our tavern for your event</h2></Row>
+      <Col xs={12}><Row className="border-bottom mb-4"><h2>Reserve our tavern for your event</h2></Row></Col>
         <Col xs={12} lg={5} style={{ height: "400px", padding: "10px" }} className="mb-3">
-          <Image style={{ height: "400px" , objectFit: "cover"}} src="../images/bar.jpg" roundedCircle fluid />
+          <Image style={{ height: "400px", objectFit: "cover" }} src="../images/bar.jpg" roundedCircle fluid />
         </Col>
         <Col xs={0} lg={2}></Col>
-        <Col xs={12} lg={5} style={{  padding: "10px" }}>
-          <Row><a style={{textDecoration: "none", margin: "0px"}} href="tel:7048128973">Call for pricing!</a></Row>
+        <Col xs={12} lg={5} style={{ padding: "10px" }}>
+          <Row><a style={{ textDecoration: "none", margin: "0px" }} href="tel:7048128973">Call for pricing!</a></Row>
           <Row><div>Perfect for:</div></Row>
           <Row><div>Birthdays</div></Row>
           <Row><div>Meetings</div></Row>
