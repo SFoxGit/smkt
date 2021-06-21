62baf979e3d22d1fbcdcfe0df1f64227da6f8f08
Shawn Fox <foxeasttrojan06@gmail.com>
2021-06-20 23:11:04 -0400

    contact spacing

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -8,14 +8,14 @@ export default function Contact() {
   return (
     <Container>
       <Row style={{padding: "10px"}}>
-        <Col xs={12} lg={6} style={{ height: "400px" }}>
-          <Row className="border-bottom"><h2>South Main Kitchen & Tavern</h2></Row>
-          <Row>128 South Main Street</Row>
-          <Row>Mount Holly, North Carolina 28120</Row>
+          <Row className="border-bottom mb-4"><h2>South Main Kitchen & Tavern</h2></Row>
+        <Col xs={12} lg={6} className="mb-3">
+          <Row><div>128 South Main Street</div></Row>
+          <Row><div>Mount Holly, North Carolina 28120</div></Row>
           <Row><a href="tel:7048128973">(704) 812-8973</a></Row>
           <Row></Row>
         </Col>
-        <Col xs={12} lg={6} style={{ height: "400px" }}>
+        <Col xs={12} lg={6} style={{ height: "400px" }} className="mb-3">
           <MapContainer style={{ height: "400px", width: "100%" }} center={[35.29629, -81.01604]} zoom={16} scrollWheelZoom={true}>
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -27,8 +27,8 @@ export default function Contact() {
             </Marker>
           </MapContainer>
         </Col>
+          <Row className="border-bottom d-flex justify-content-center mb-4"><h2>Hours</h2></Row>
         <Col xs={12} lg={6} style={{ height: "400px" }}>
-          <Row className="border-bottom d-flex justify-content-center"><h2>Hours</h2></Row>
           <Row><Col className="days" xs={4}>Monday</Col> <Col className="timeOpen" xs={8}>11:00 AM - 9:00 PM</Col></Row>
           <Row><Col className="days" xs={4}>Tuesday</Col> <Col className="timeOpen" xs={8}>11:00 AM - 9:00 PM</Col></Row>
           <Row><Col className="days" xs={4}>Wednesday</Col> <Col className="timeOpen" xs={8}>11:00 AM - 9:00 PM</Col></Row>
